d353a7a1431922a500d6c1588327a739b20796bb
j-beaumont <89831161+j-beaumont@users.noreply.github.com>
2025-04-15 10:31:02 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,31 @@
-/**     // TO DO
- * @file main.cpp
- * @author your name
- * @brief 
- * @version 0.1
- * @date 2023-01-03
- * 
- * @copyright Copyright (c) 2023
- * 
- */
-//////////////////////////////////////////////////////////////////////
+/*      Programme d'affichage d'image
+
+Date : 14/04/25
+Rev.: 1.0
+Auteur: J.B.
+*/
+//////////////////////////////////////////////////////////////////////////////////////////
 
 #include <Arduino.h>
+#include "affichage.h"
+#include "images.h"
 
-//////////////////////////////////////////////////////////////////////
-void setup() {
-   // TO DO
- }
-//////////////////////////////////////////////////////////////////////
-void loop() {
-  // TO DO
+/////////////////////////////////////////////////////////////////////////////
+void setup()
+{
+  initAfficheur();
+  effacerAfficheur();
+}
+
+void loop()
+{
+  afficher(LIGNE_1, "Cybersecurite");
+  afficher(LIGNE_2, "Informatique");
+  afficher(LIGNE_3, "ELectronique");
+  afficher(LIGNE_4, "& reseaux");
+  delay(2000);
+  effacerAfficheur();
+  afficherImage(avengers);
+  delay(2000);
+  effacerAfficheur();
 }
